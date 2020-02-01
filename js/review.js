894fd1cc50c3ceab54a52a41f77d12de808bc1f3
Groverkss <groverkss@gmail.com>
2020-02-01 17:48:15 +0530

    added review page

--- js/review.js
+++ js/review.js
@@ -9,3 +9,17 @@ function revealMe() {
         revealState = false;
     }
 }
+
+function createNewValue() {
+    revealMe();
+    var name = document.getElementById("form-name").value;
+    var skill = document.getElementById("form-skill").value;
+    var range = document.getElementById("form-range").value;
+    var rangeInt = parseInt(range);
+    var comment = document.getElementById("form-comments").value;
+    document.getElementById("table-body").innerHTML += "<tr><td>" + name + "</td><td>" + skill + "</td><td>" + rangeInt + "</td><td>" + comment + "</td></tr>";
+    document.getElementById("form-name").value = "";
+    document.getElementById("form-skill").value = "";
+    document.getElementById("form-range").value = "";
+    document.getElementById("form-comments").value = "";
+}
