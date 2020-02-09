4bd8b66a9d1f68a0e1c4a10072b1ad84fdb064a7
Groverkss <groverkss@gmail.com>
2020-02-09 08:12:09 +0530

    Updated file links

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,6 @@ function goTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+document.getElementById("head-head").style.position = "fixed";
+document.getElementById("head-head").style.top = "0";
--- js/review.js
+++ js/review.js
@@ -10,6 +10,7 @@ function revealMe() {
     }
 }
 
+
 function createNewValue() {
     revealMe();
     var name = document.getElementById("form-name").value;
@@ -25,9 +26,41 @@ function createNewValue() {
     } else {
         rangeAns = "Proficient";
     }
+
+    
     document.getElementById("table-body").innerHTML += "<tr><td>" + name + "</td><td>" + skill + "</td><td>" + rangeAns + "</td><td>" + comment + "</td></tr>";
     document.getElementById("form-name").value = "";
     document.getElementById("form-skill").value = "";
     document.getElementById("form-range").value = "";
     document.getElementById("form-comments").value = "";
+    
+    var obj = {
+        name: name,
+        skill: skill,
+        range: rangeAns,
+        comment: comment
+    };
+    
+
+    var retdat = localStorage.getItem("JSONdat");
+    var data = JSON.parse(retdat);
+    if(data == null){
+    data = [];
+    }
+    data.push(obj);
+    console.log(obj);
+    console.log(data);
+    localStorage.setItem("JSONdat", JSON.stringify(data));
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var retdat = localStorage.getItem("JSONdat");
+    var data = JSON.parse(retdat);
+    if(data == null){
+    data = [];
+    }
+
+    data.forEach(function(obj) {
+        document.getElementById("table-body").innerHTML += "<tr><td>" + obj.name + "</td><td>" + obj.skill + "</td><td>" + obj.range + "</td><td>" + obj.comment + "</td></tr>";
+    })
+})
